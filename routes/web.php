d5c49790e009c23de801aedcd8466df7a22ae7b2
uiahan <farhandikaalsani@gmail.com>
2025-02-09 00:43:50 +0700

    koreksi route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
 
 Route::get('/admin/dashboard', function () {
     return view('pages.admin.dashboard');
-});
-
-Route::get('/login', function () {
-    return view('pages.auth.login');
 });
