23542059d6c8e0e3302e266c92e349d0bdeaebfb
NaraMizaru <hilal.muhammad0807@gmail.com>
2025-02-08 21:04:32 +0700

    setup: admin dashboard

---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,34 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fa-regular fa-fw fa-envelope"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">E-Voting <sup>2.0</sup></div>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item">
+        <a href="" class="nav-link">
+            <i class="fa-regular fa-fw fa-house"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">Kelola</div>
+
+    <li class="nav-item">
+        <a href="{{ route('admin.manage.class') }}" class="nav-link">
+            <i class="fa-regular fa-fw fa-school"></i>
+            <span>Kelola Kelas</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
