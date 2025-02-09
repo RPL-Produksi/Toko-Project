ab7b4a3fe2fe88a73f2c40baae75a12059713cd3
NaraMizaru <hilal.muhammad0807@gmail.com>
2025-02-09 10:16:49 +0700

    fix: conflict route

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use App\Http\Controllers\admin\AdminController;
 use App\Http\Controllers\owner\OwnerController;
 use App\Http\Controllers\superadmin\SuperAdminController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('pages.auth.login');
 });
 
+Route::prefix('kasir')->group(function () {
+    Route::inertia('/dashboard', 'Dashboard');
+});
 
 Route::get('/superadmin/dashboard', [SuperAdminController::class, 'dashboard'])->name('superadmin.dashboard');
 Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 Route::get('/owner/dashboard', [OwnerController::class, 'dashboard'])->name('owner.dashboard');
-
