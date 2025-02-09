d4fad087e6ee1ac93a68e22758e3c9c49caa5fdb
NaraMizaru <hilal.muhammad0807@gmail.com>
2025-02-09 12:28:11 +0700

    change: id to uuid

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('nama_lengkap');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['superadmin', 'admin', 'owner', 'kasir'])->default('kasir');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2025_02_09_100937_create_pesanans_table.php
+++ database/migrations/2025_02_09_100937_create_pesanans_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pesanans', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->timestamps();
         });
     }
