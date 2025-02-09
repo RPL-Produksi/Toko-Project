a21c984882729ddd15bd7cf6c68004f6cc250ac6
NaraMizaru <hilal.muhammad0807@gmail.com>
2025-02-09 10:10:00 +0700

    add: menambahkan beberapa model dan migration

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <section class="login-section">
         <div class="row h-100">
-            <div class="col-md-7 d-none d-md-block" style="background-color: #4e72de">
+            <div class="col-md-7 d-none d-md-block bg-primary">
                 <div class="d-flex align-items-center justify-content-center" style="height: 100vh">
                     <div>
                         <div class="d-flex justify-content-center mb-5">
@@ -16,7 +16,7 @@
                                 alt="">
                         </div>
                         <div class="text-white">
-                            <h3 class="text-center fw-semibold">Selamat Datang di Lumi Store</h3>
+                            <h3 class="text-center font-weight-bold">Selamat Datang di Lumi Store</h3>
                             <p class="text-center">Aplikasi ini dirancang untuk memudahkan Anda dalam mengelola dan <br>
                                 memantau perkembangan toko Lumi Store.</p>
                         </div>
@@ -26,7 +26,7 @@
             <div class="col-md-5 d-flex justify-content-center align-items-center" style="background-color: white">
                 <div class="login-form">
                     <div>
-                        <h2 class="fw-bold text-center" style="color: #666666">Lumi Store</h2>
+                        <h2 class="font-weight-bold text-center" style="color: #666666">Lumi Store</h2>
                     </div>
                     <div class="mt-4">
                         <h3 class="text-center" style="color: #777777">Hallo! Selamat Datang</h3>
@@ -43,12 +43,11 @@
                                 <input type="password" name="password" required class="form-control">
                             </div>
                             <div class="d-flex justify-content-end">
-                                <a class="mt-2" style="font-size: 14px; text-decoration: none; color: #4e72de;">RPL
+                                <a class="mt-2 text-primary" style="font-size: 14px; text-decoration: none;">RPL
                                     Production</a>
                             </div>
                             <div class="d-flex justify-content-center mt-4">
-                                <button type="submit" class="btn btn-login py-2 rounded-5 text-white"
-                                    style="background-color: #4e72de">Masuk</button>
+                                <button type="submit" class="btn btn-login py-2 rounded-5 bg-primary text-white">Masuk</button>
                             </div>
                         </form>
                     </div>
